d745a0812c28f7219ea243b8f83f4218ee6cdbe0
Ivaylo Vasilev <ivo4j@users.noreply.github.com>
2024-11-20 11:23:15 +0200

    Changed the first two folders to Types and Conditionals

--- Exercises1/Exercises1/obj/Debug/net8.0/Exercises1.AssemblyInfo.cs
+++ Exercises1/Exercises1/obj/Debug/net8.0/Exercises1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercises1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47bf0c94ba3565d2a8ba9da4eb0d27b10b041d9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcd5e3c66ca3af12508ea98f989cd9a6d0dae320")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercises1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercises1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

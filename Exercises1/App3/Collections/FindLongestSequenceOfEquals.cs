09e215fdbe8abe59152349fd7a826c217414d65f
Ivaylo Vasilev <ivo4j@users.noreply.github.com>
2024-12-08 20:43:59 +0200

    Added Find the Longest sequence / App3

--- Exercises1/App3/Collections/FindLongestSequenceOfEquals.cs
+++ Exercises1/App3/Collections/FindLongestSequenceOfEquals.cs
@@ -11,10 +11,56 @@ namespace App3.Collections
         public void FindLongestSequence()
         {
             Console.WriteLine("Please enter a sequence of numbers separated by space: ");
-            string numbers = Console.ReadLine();
+            string numbersInput = Console.ReadLine();
+
+            string[] seqNumbers = numbersInput.Split();
+
+            int[] numbers = Array.ConvertAll(seqNumbers, int.Parse);
+
+            // Variables to track the longest sequence
+            int maxLength = 1;
+            int maxStartIndex = 0;
+
+            // Variables for the current sequence
+            int currentLength = 1;
+            int currentStartIndex = 0;
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] == numbers[i - 1])
+                {
+                    currentLength++;
+                }
+                else
+                {
+                    // Check if the current sequence is the longest
+                    if (currentLength > maxLength)
+                    {
+                        maxLength = currentLength;
+                        maxStartIndex = currentStartIndex;
+                    }
+
+                    // Reset for the new sequence
+                    currentLength = 1;
+                    currentStartIndex = i;
+                }
+            }
+
+            // Final check for the last sequence
+            if (currentLength > maxLength)
+            {
+                maxLength = currentLength;
+                maxStartIndex = currentStartIndex;
+            }
+
+            // Output the longest sequence
+            Console.WriteLine("Longest sequence:");
+            for (int i = 0; i < maxLength; i++)
+            {
+                Console.Write(numbers[maxStartIndex + i] + " ");
+            }
 
 
-        
         }
     }
 }
--- Exercises1/App3/obj/Debug/net8.0/App3.AssemblyInfo.cs
+++ Exercises1/App3/obj/Debug/net8.0/App3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90375f70d2646a95301681dc47716c843a5cf6b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5693413f7087c0d7afa3e3edbb714a8359da3a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("App3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

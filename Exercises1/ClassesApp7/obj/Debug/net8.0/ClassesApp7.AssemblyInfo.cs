b169fc5b83ac64be8e2afd208c0653e62a6070c3
Ivaylo Vasilev <ivo4j@users.noreply.github.com>
2025-02-07 15:10:22 +0200

    Added RestSharpCAllsApp - Homework for Lecture 12

--- Exercises1/ClassesApp7/obj/Debug/net8.0/ClassesApp7.AssemblyInfo.cs
+++ Exercises1/ClassesApp7/obj/Debug/net8.0/ClassesApp7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassesApp7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b42b157fac9d891556c3387e869727e088c2794")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66820bade18e18ae2d1f93aab89489cac8122816")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassesApp7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassesApp7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Exercises1/LibraryManagementSystem/obj/Debug/net8.0/LibraryManagementSystem.AssemblyInfo.cs
+++ Exercises1/LibraryManagementSystem/obj/Debug/net8.0/LibraryManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b42b157fac9d891556c3387e869727e088c2794")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66820bade18e18ae2d1f93aab89489cac8122816")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

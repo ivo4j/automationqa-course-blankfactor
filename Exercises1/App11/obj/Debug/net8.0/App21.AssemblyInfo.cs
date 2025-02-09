dcbaa2f5f9fae3bd63ae2b3613787a635937eaaf
Ivaylo Vasilev <ivo4j@users.noreply.github.com>
2025-02-09 17:03:54 +0200

    Starting a project for the last lecture - Lecture 15, JPathFilesApp15

--- Exercises1/App11/obj/Debug/net8.0/App21.AssemblyInfo.cs
+++ Exercises1/App11/obj/Debug/net8.0/App21.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App21")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0a68f866307a2d05d9204ba8d00472b22c6991c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6f2a04c62465974199c4d2b269a806002fe0138")]
 [assembly: System.Reflection.AssemblyProductAttribute("App21")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App21")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Exercises1/JSONJpathApp14/obj/Debug/net8.0/JSONJpathApp14.AssemblyInfo.cs
+++ Exercises1/JSONJpathApp14/obj/Debug/net8.0/JSONJpathApp14.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JSONJpathApp14")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf713cba9f446d9be713381edb4fd444f2180a27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6f2a04c62465974199c4d2b269a806002fe0138")]
 [assembly: System.Reflection.AssemblyProductAttribute("JSONJpathApp14")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JSONJpathApp14")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
